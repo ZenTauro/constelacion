022cc6f26553bf822409f629e5356a9e7a9fa289
zentauro <pedrogomezmartin1999@gmail.com>
2019-04-11 11:27:52 +0200

    Matlab script and figures

--- media/images.m
+++
@@ -1,9 +0,0 @@
-[x, y, z] = linspace (-5, 5, 50)';
-[xx , yy] = meshgrid(x, y);
-
-r = 1 ./ (xx .^2 + yy .^2) + eps;
-mesh(xx, yy, r)
-
-vx = -2 * ( xx ./ ((xx) .^2 + (yy).^2 ) );
-vy = -2 * ( yy ./ ((xx) .^2 + (yy).^2 ) );
-quiver(xx, yy, vx, vy)
---
+++ media/superpos.m
@@ -0,0 +1,27 @@
+function none = superpos(points)
+  x = y = z = linspace (-5, 5, 50)';
+  [xx , yy] = meshgrid(x, y);
+
+  r = 1 ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  r -= 1 ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  for i = 1:length(points)
+    r += 1 ./ ((xx - points{i}.x) .^2 + (yy - points{i}.y ) .^2) + eps;
+  endfor
+  f = figure;
+  mesh(xx, yy, r)
+  pbaspect([1; 1; 1])
+  print -dtikz "mesh.tikz"
+
+  vx = -2 * (xx - points{1}.x) ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  vx -= -2 * (xx - points{1}.x) ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  vy = -2 * (yy - points{1}.y) ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  vy -= -2 * (yy - points{1}.y) ./ ((xx - points{1}.x) .^2 + (yy - points{1}.y ) .^2) + eps;
+  for i = 1:length(points)
+    vx += -2 * (xx - points{i}.x) ./ ((xx - points{i}.x) .^2 + (yy - points{i}.y ) .^2) + eps;
+    vy += -2 * (yy - points{i}.y) ./ ((xx - points{i}.x) .^2 + (yy - points{i}.y ) .^2) + eps;
+  endfor
+  v = figure;
+  quiver(xx, yy, vx, vy)
+  pbaspect([1, 1])
+  print -dtikz "vec.tikz"
+endfunction
